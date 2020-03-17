33ca53effac5ca31e1d9c69e7b70b193d99227f8
Siddhartha Saxena <siddharthasaxena.1998@gmail.com>
2020-03-17 15:27:33 +0530

    Implementing React

---
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Leads extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Leads Component</h1>
+            </div>
+        )
+    }
+}
+
+export default Leads;
